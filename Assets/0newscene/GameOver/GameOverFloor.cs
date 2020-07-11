62ce4444c36711ec9eaad8ec5ee87a85c84bbe25
wesghi <66157235+wesghi@users.noreply.github.com>
2020-07-11 15:31:32 +0900

    si-ntukuttayo

---
+++ Assets/0newscene/GameOver/GameOverFloor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ★追加
+using UnityEngine.SceneManagement;
+
+public class GameOverFloor : MonoBehaviour
+{
+    // ★追加
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 「Player」と「GameOver」の大文字・小文字に注意。
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/0newscene/Title/Title.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ★追加
+using UnityEngine.SceneManagement;
+
+public class Title: MonoBehaviour
+{
+    // ★追加
+    // 「public」を必ずつけること（ポイント）
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("ForMrDanScene");
+    }
+}
