520c6fd666fa1a2419a2eed90df1815f70741a80
wesghi <66157235+wesghi@users.noreply.github.com>
2020-07-14 21:59:35 +0900

    saishuutyousei

---
+++ Assets/0newscene/ReturnToTitle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ★追加
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+    // ★追加
+    public int timeCount;
+
+    void Start()
+    {
+        // ★追加
+        // 任意に設定した時間の経過後、「GoTitle」メソッドを呼び出す（ポイント）
+        Invoke("GoTitle", timeCount);
+    }
+
+    // ★追加
+    void GoTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
